ed097f8f2ccd0b052a46d942b9101ab4d2e125b0
M Bussonnier <bussonniermatthias@gmail.com>
2025-03-13 10:30:38 +0100

    Fix some async do completes.
    Some versions of pytest do auto run async tests,
    recents one do not, and marking with
    `@pytest.mark.anyio` also test with multiple
    runner if installed.
    
    Handle case where do_complete is both async and
    sync, and make _do_complete always async.
    
    I only found mentions of the private _do_complete
    in spyder kernel, so if present somewhere else you
    might need to lift the if awaitable one level up.

--- spyder_kernels/console/tests/test_console_kernel.py
+++ spyder_kernels/console/tests/test_console_kernel.py
@@ -874,14 +874,16 @@ def test_matplotlib_inline(kernel):
         # Assert backend is inline
         assert 'inline' in value
 
-
-def test_do_complete(kernel):
+@pytest.mark.anyio
+async def test_do_complete(kernel):
     """
     Check do complete works in normal and debugging mode.
     """
-    asyncio.run(kernel.do_execute('abba = 1', True))
-    assert kernel.get_value('abba') == 1
-    match = kernel.do_complete('ab', 2)
+    await kernel.do_execute("abba = 1", True)
+    assert kernel.get_value("abba") == 1
+    match = kernel.do_complete("ab", 2)
+    if inspect.isawaitable(match):
+        match = await match
     assert 'abba' in match['matches']
 
     # test pdb
@@ -890,6 +892,8 @@ def test_do_complete(kernel):
     pdb_obj.completenames = lambda *ignore: ['baba']
     kernel.shell._namespace_stack = [pdb_obj]
     match = kernel.do_complete('ba', 2)
+    if inspect.isawaitable(match):
+        match = await match
     assert 'baba' in match['matches']
     pdb_obj.curframe = None
 
